ea7e72af43cda853d67d089bb2410751ddeece4e
Konrad Kubrak <kubrakkonrad@gmail.com>
2023-05-09 22:46:20 +0200

    Rewrite room gallery component to funcition

--- src/RoomGallery.js
+++ src/RoomGallery.js
@@ -1,132 +1,111 @@
 import './sass/formir-room.scss'
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import Room from './components/Room'
 import { parseRooms, parseWalls } from './helpers/parse'
 
-export default class RoomGallery extends Component {
-  constructor (props) {
-    super(props)
-    this.preRooms = []
-    this.preItems = []
-    this.state = {
-      items: this.preItems,
-      rooms: this.preRooms,
-      position: { x: 0, y: 0 },
-      dark: false,
-      zoom: false,
-      activeWall: 0,
-      activeItem: null
-    }
+const RoomGallery = () => {
+  const [currentState, setCurrentState] = useState({ items: [], rooms: [], activeItem: null })
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+  const [dark, setDark] = useState(false)
+  const [zoom, setZoom] = useState(false)
+
+  const preRooms = []
+  const preItems = []
+
+  const parseItems = (dataItems) => {
+    parseRooms(dataItems, preRooms)
+    const activeItem = parseWalls(dataItems, preItems, preRooms)
+    setCurrentState({ rooms: preRooms, items: preItems, activeItem })
   }
 
-  parseItems = (items) => {
-    this.items = items
-    parseRooms(this.items, this.preRooms)
-    const activeItem = parseWalls(this.items, this.preItems, this.preRooms)
-    this.setState({ activeItem })
-  }
-
-  componentDidMount () {
-    fetch('data.json')
-      .then(response => response.json())
-      .then(data => this.parseItems(data.items))
-  }
-
-  nextItem = () => {
-    if (this.state.activeItem.id < this.state.items.length) {
-      this.setCurrent(this.state.items[this.state.activeItem.id + 1])
+  const nextItem = () => {
+    if (currentState.activeItem.id < currentState.items.length) {
+      setCurrent(currentState.items[currentState.activeItem.id + 1])
     } else {
-      this.setCurrent(this.state.items[this.state.items.length])
+      setCurrent(currentState.items[currentState.items.length])
     }
   }
 
-  prevItem = () => {
-    if (this.state.activeItem.id > 0) {
-      this.setCurrent(this.state.items[this.state.activeItem.id - 1])
+  const prevItem = () => {
+    if (currentState.activeItem.id > 0) {
+      setCurrent(currentState.items[currentState.activeItem.id - 1])
     } else {
-      this.setCurrent(this.state.items[0])
+      setCurrent(currentState.items[0])
     }
   }
 
-  currentItem = () => {
-    return this.state.activeItem
+  const currentItem = () => {
+    return currentState.activeItem
   }
 
-  setCurrent = (item) => {
-    this.setState(function () {
-      return {
-        activeItem: item,
-        position: { y: item.position.y, x: item.position.x }
-      }
-    })
+  const setCurrent = (item) => {
+    setCurrentState({ items: currentState.items, rooms: currentState.rooms, activeItem: item })
+    setPosition({ y: item.position.y, x: item.position.x })
   }
 
-  darkModeToggle = () => {
-    this.setState(function (prevState) {
-      return {
-        dark: !prevState.dark
-      }
-    })
+  const darkModeToggle = () => {
+    setDark(!dark)
   }
 
-  zoomToggle = () => {
-    this.setState(function (prevState) {
-      return {
-        zoom: !prevState.zoom
-      }
-    })
+  const zoomToggle = () => {
+    setZoom(!zoom)
   }
 
-  render () {
-    const { prevItem, nextItem, currentItem, setCurrent, darkModeToggle, zoomToggle } = this
-    const {
-      state: {
-        rooms, items, dark, zoom
-      }
-    } = this
-    return (
-      <>
-        { rooms.length > 0 && <div className={`room ${dark ? 'room-dark' : ''} ${zoom ? 'room-zoom' : ''}`}>
-            <div className="room-body">
-              <div className="room-arena">
-                { rooms.map(room => (
-                  <Room
-                    key={room.id}
-                    room={room}
-                    rooms={rooms}
-                    position={this.state.position}/>
-                )) }
-              </div>
-              <div className="room-navigations">
-                { this.state.activeItem.id > 0 &&
-                  <button className="prev" onClick={prevItem}>
-                    <span>{currentItem().id}</span>
-                  </button>
-                }
-                {
-                  this.state.items.length > this.state.activeItem.id + 1 &&
-                  <button className="next" onClick={nextItem}>
-                    <span>{currentItem().id + 2.0}</span>
-                  </button>
-                }
+  useEffect(() => {
+    const dataFetch = async () => {
+      const data = await (
+        await fetch('data.json')
+      ).json()
+      parseItems(data.items)
+    }
 
-              </div>
-              <div className="room-paginations">
-                { items.map(item => (
-                  item.image && <button
-                    key={item.id}
-                    onClick={() => setCurrent(item) }>
-                      {item.id + 1}
-                    </button>
-                ))}
-              </div>
-              <button className="room-exit-btn" onClick={zoomToggle}></button>
-              <button className="room-zoom-btn" onClick={zoomToggle}></button>
-              <button className="room-dark-btn" onClick={darkModeToggle}></button>
+    dataFetch()
+  }, [])
+
+  return (
+    <>
+      { currentState.rooms.length > 0 && <div className={`room ${dark ? 'room-dark' : ''} ${zoom ? 'room-zoom' : ''}`}>
+          <div className="room-body">
+            <div className="room-arena">
+              { currentState.rooms.map(room => (
+                <Room
+                  key={room.id}
+                  room={room}
+                  rooms={currentState.rooms}
+                  position={position}/>
+              )) }
             </div>
+            <div className="room-navigations">
+              { currentState.activeItem.id > 0 &&
+                <button className="prev" onClick={() => prevItem()}>
+                  <span>{currentItem().id}</span>
+                </button>
+              }
+              {
+                currentState.items.length > currentState.activeItem.id + 1 &&
+                <button className="next" onClick={() => nextItem()}>
+                  <span>{currentItem().id + 2.0}</span>
+                </button>
+              }
+
+            </div>
+            <div className="room-paginations">
+              { currentState.items.map(item => (
+                item.image && <button
+                  key={item.id}
+                  onClick={() => setCurrent(item) }>
+                    {item.id + 1}
+                  </button>
+              ))}
+            </div>
+            <button className="room-exit-btn" onClick={() => zoomToggle()}></button>
+            <button className="room-zoom-btn" onClick={() => zoomToggle()}></button>
+            <button className="room-dark-btn" onClick={() => darkModeToggle()}></button>
           </div>
-        }
-      </>
-    )
-  }
+        </div>
+      }
+    </>
+  )
 }
+
+export default RoomGallery
