51ea988e4ccfaca24b6c3f3f2865f06fbc91ea25
Konrad Kubrak <kubrakkonrad@gmail.com>
2023-11-07 23:33:29 +0100

    Expose docs dir on server

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'docs')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'docs', 'index.html'));
 });
 
 app.listen(port, () => {
