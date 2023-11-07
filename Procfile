71a8b2986711e3ee0cb57feacb5fa11d6341e9cb
Konrad Kubrak <kubrakkonrad@gmail.com>
2023-11-07 10:35:49 +0100

    Add index to dist, add procfile - develop mode for heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: webpack serve --mode development $PORT
---
+++ dist/index.d.ts
@@ -0,0 +1,5 @@
+import type { RoomGalleryProps } from './types/types';
+import { RoomGallery } from './components/RoomGallery';
+export { RoomGallery, RoomGalleryProps };
+export default RoomGallery;
+//# sourceMappingURL=index.d.ts.map
